20e5a88f8d8aa56081b8702a92630dfc95532c31
Andrea Marco Sartori <22cerbero22@gmail.com>
2014-02-06 23:39:03 +0100

    Italian translation added

---
+++ src/lang/it/users.php
@@ -0,0 +1,53 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| User Repositiory Messages
+	|--------------------------------------------------------------------------
+	*/
+
+	'created' 		=>  "Il tuo account è stato creato. Controlla il tuo indirizzo eMail per il link di conferma.",	
+
+	'loginreq' 		=>	"Il campo di login è obbligatorio.",
+
+	'exists'		=>	"L'utente esiste già.",
+
+	'notfound'		=>	"Utente non trovato",
+
+	'noaccess'		=>	"Non hai il permesso di compiere questa azione.",
+
+	'updated'		=>	"Profilo aggiornato",
+
+	'notupdated'	=>	"Impossibile aggiornare il profilo",
+
+	'activated'		=>	"Attivazione completata. <a href=':url' class='alert-link'>Puoi effettuare il login</a>",
+
+	'notactivated'	=>	"L'attivazione non può essere completata.",
+
+	'alreadyactive'	=>	"L'account è già stato attivato.",
+
+	'emailconfirm'	=>	"Controlla il tuo indirizzo eMail per il link di conferma.",
+
+	'emailinfo'		=>	"Controlla il tuo indirizzo eMail per le istruzioni.",
+
+	'emailpassword'	=>	"La tua password è stata cambiata. Controlla il tuo indirizzo eMail per la nuova password.",
+
+	'problem'		=>	"Si è verificato un problema. Per favore contatta l'amministratore.",
+
+	'passwordchg'	=> 	"La tua password è stata cambiata.",
+
+	'passwordprob'	=>	"La tua password non può essere cambiata.",
+
+	'oldpassword'	=>	"Non hai inserito correttamente la password originale.",
+
+	'suspended'		=>	"L'utente è stato sospeso per :minutes minuti.",
+
+	'unsuspended'	=>	"Sospensione rimossa.",
+
+	'banned'		=>	"L'utente è stato bannato.",
+
+	'unbanned'		=>	"Il ban dell'utente è stato rimosso.",
+
+);
