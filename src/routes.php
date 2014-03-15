4af38d9f62a59d7d59087768097c8add96b3dc6e
Adis <adis.osmonov@gmail.com>
2014-03-16 00:17:15 +0600

    removed Route::pattern('id', '[0-9]+');
    This pattern conflicts with other route parameters

--- src/routes.php
+++ src/routes.php
@@ -29,7 +29,8 @@ Route::resource( $sessions , 'Sentinel\SessionController', array('only' => array
 
 // User Routes
 Route::get( $register , array('as' => 'Sentinel\register', 'uses' => 'Sentinel\UserController@register'));
-Route::get( $users . '/{id}/activate/{code}', 'Sentinel\UserController@activate');
+Route::get( $users . '/{id}/activate/{code}', 'Sentinel\UserController@activate')
+->where('id', '[0-9]+');
 Route::get( $resend , array('as' => 'Sentinel\resendActivationForm', function()
 {
 	return View::make('Sentinel::users.resend');
@@ -40,16 +41,23 @@ Route::get( $forgot , array('as' => 'Sentinel\forgotPasswordForm', function()
 	return View::make('Sentinel::users.forgot');
 }));
 Route::post( $forgot , 'Sentinel\UserController@forgot');
-Route::post( $users . '/{id}/change', 'Sentinel\UserController@change');
-Route::get( $users . '/{id}/reset/{code}', 'Sentinel\UserController@reset');
+Route::post( $users . '/{id}/change', 'Sentinel\UserController@change')
+->where('id', '[0-9]+');
+Route::get( $users . '/{id}/reset/{code}', 'Sentinel\UserController@reset')
+->where('id', '[0-9]+');
 Route::get( $users . '/{id}/suspend', array('as' => 'Sentinel\suspendUserForm', function($id)
 {
 	return View::make('Sentinel::users.suspend')->with('id', $id);
-}));
-Route::post( $users . '/{id}/suspend', 'Sentinel\UserController@suspend');
-Route::get( $users . '/{id}/unsuspend', 'Sentinel\UserController@unsuspend');
-Route::get( $users . '/{id}/ban', 'Sentinel\UserController@ban');
-Route::get( $users . '/{id}/unban', 'Sentinel\UserController@unban');
+}))
+->where('id', '[0-9]+');
+Route::post( $users . '/{id}/suspend', 'Sentinel\UserController@suspend')
+->where('id', '[0-9]+');
+Route::get( $users . '/{id}/unsuspend', 'Sentinel\UserController@unsuspend')
+->where('id', '[0-9]+');
+Route::get( $users . '/{id}/ban', 'Sentinel\UserController@ban')
+->where('id', '[0-9]+');
+Route::get( $users . '/{id}/unban', 'Sentinel\UserController@unban')
+->where('id', '[0-9]+');
 Route::resource( $users , 'Sentinel\UserController');
 
 // Group Routes
