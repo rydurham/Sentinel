21c26aa9b3b24ddd51bee0c0cce0d8dd28cc0d7d
Ryan Durham <rydurham@gmail.com>
2014-05-12 15:06:03 -0700

    Added BG language files

---
+++ src/lang/bg/pages.php
@@ -0,0 +1,65 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Language strings for views
+	|--------------------------------------------------------------------------
+	*/
+
+	'flashsuccess'	=> 'Успех',
+
+	'flasherror'	=> 'Грешка',
+
+	'flashwarning'	=> 'Предупреждение',
+
+	'flashinfo'		=> 'Разбери защо',
+
+	'register'		=> 'Регистрирай',
+
+	'login'			=> "Вход",
+
+	'logout'		=> "Изход",
+
+	'home'			=> "Начало",
+
+	'users'			=> "Потребител|Потребители",
+
+	'groups'		=> "Групи",
+
+	'helloworld'	=> "Здравейте",
+
+	'description'	=> "This is an example of <a href=\"https://github.com/laravel/laravel\">Laravel 4.1</a> running with <a href=\"https://github.com/cartalyst/sentry\">Sentry 2.0</a> and <a href=\"http://getbootstrap.com/\">Bootstrap 3.0</a>.",
+
+	'loginstatus'	=> "Вие сте влезли в системата.",
+
+	'sessiondata'	=> "Информация за сесията",
+
+	'currentusers'	=> "Потребители",
+
+	'options'		=> 'Настройки',
+
+	'status'		=> "Статус",
+
+	'active'		=> "Ативен",
+
+	'notactive'		=> "Не активен",
+
+	'suspended'		=> "Изключен",
+
+	'banned'		=> "Забранен",
+
+	'actionedit'	=> "Промени",
+
+	'actionsuspend'	=> "Изключи",
+
+	'actionunsuspend' => "Включи",
+
+	'actionban'		=> "Забрани",
+
+	'actionunban'	=> "Разреши",
+
+	'actiondelete'	=> "Изтрий",
+
+);
