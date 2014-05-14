d4a6378d89f2f4287f66f199e0400e1971876b21
Sunel <sunelbe@gmail.com>
2014-05-14 18:07:50 +0530

    added the additional files option

--- src/views/users/create.blade.php
+++ src/views/users/create.blade.php
@@ -16,13 +16,30 @@ Register
 
                 @foreach (Config::get('Sentinel::config.newfields') as $field => $lable)
 
-                    <div class="form-group {{ ($errors->has($field)) ? 'has-error' : '' }}">
-                        {{ Form::text($field, null, array('class' => 'form-control', 'placeholder' => $lable)) }}
-                        {{ ($errors->has($field) ? $errors->first($field) : '') }}
-                    </div>
+                <div class="form-group {{ ($errors->has($field)) ? 'has-error' : '' }}">
+                    {{ Form::text($field, null, array('class' => 'form-control', 'placeholder' => $lable)) }}
+                    {{ ($errors->has($field) ? $errors->first($field) : '') }}
+                </div>
                
                @endforeach
-            
+    
+                
+    
+                <div class="form-group {{ ($errors->has('email')) ? 'has-error' : '' }}">
+                    {{ Form::text('email', null, array('class' => 'form-control', 'placeholder' => 'E-mail')) }}
+                    {{ ($errors->has('email') ? $errors->first('email') : '') }}
+                </div>
+    
+                <div class="form-group {{ ($errors->has('password')) ? 'has-error' : '' }}">
+                    {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) }}
+                    {{ ($errors->has('password') ?  $errors->first('password') : '') }}
+                </div>
+    
+                <div class="form-group {{ ($errors->has('password_confirmation')) ? 'has-error' : '' }}">
+                    {{ Form::password('password_confirmation', array('class' => 'form-control', 'placeholder' => 'Confirm Password')) }}
+                    {{ ($errors->has('password_confirmation') ?  $errors->first('password_confirmation') : '') }}
+                </div>
+                
             {{ Form::submit('Register', array('class' => 'btn btn-primary')) }}
             
         {{ Form::close() }}
