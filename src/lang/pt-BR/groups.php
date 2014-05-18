48548963a9705af7cdba9f879c673523b96498ea
Elias Rodrigues <elias@binaryweb.com.br>
2014-05-17 23:41:38 -0300

    Translation for  Brazilian Portuguese / pt-BR

---
+++ src/lang/pt-BR/groups.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Group Repositiory Messages
+	|--------------------------------------------------------------------------
+	*/
+
+	'created' 		=> "Grupo Criado.",
+
+	'loginreq' 		=> "O campo Login é obrigatório.",
+
+	'userexists' 	=> "Usuário já existe.",
+
+	'updated' 		=> "O Grupo foi atualizado.",
+
+	'updateproblem' => "Ocorreu um problema ao atualizar o grupo.",
+
+	'namereq' 		=> "Você deve informar um nome para o grupo.",
+
+	'groupexists' 	=> "Este grupo já existe.",
+
+	'notfound' 		=> "Grupo não encontrado.",
+
+);
---
+++ src/lang/pt-BR/sessions.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Session Repository Messages
+	|--------------------------------------------------------------------------
+	*/
+
+	"invalid"   => "Usuário ou Senha inválidos.",
+
+	"notactive" =>  "Você ainda não ativou sua conta. <a href=':url' class='alert-link'>Reenviar Email de Ativação?</a>",
+
+	"suspended" => "Sua conta foi suspensa temporariamente.",
+
+	"banned" 	=> "Você está banido.",
+
+);
