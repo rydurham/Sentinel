9dd605efa42e66d907b45af3dd211834043f29cd
llagrimas <llagrimas@stratpoint.com>
2014-07-17 18:35:31 +0800

    modify to required spiels

--- src/lang/en/sessions.php
+++ src/lang/en/sessions.php
@@ -8,12 +8,14 @@ return array(
 	|--------------------------------------------------------------------------
 	*/
 
-	"invalid"   => "Please enter a valid username and/or password.",
+	"invalid"   	=> "Please enter a valid username and/or password.",
+	
+	"notverified"	=> "Please verify this email address first to proceed. Resent confirmation email.",
 
-	"notactive" =>  "Please verify this email address first to proceed. Click <a href=':url' class='alert-link'>HERE</a> to resend the activation email.",
+	"notactive" 	=>  "Please verify this email address first to proceed. Click <a href=':url' class='alert-link'>HERE</a> to resend the activation email.",
 
-	"suspended" => "Your account has been temporarily suspended.",
+	"suspended"		=> "Your account has been temporarily suspended.",
 
-	"banned" 	=> "You have been banned.",
+	"banned" 		=> "You have been banned.",
 
 );
