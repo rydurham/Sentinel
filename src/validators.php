734d15cded858d087a08f6793ef3ce58548502f9
Ryan Durham <rydurham@gmail.com>
2014-08-11 01:26:50 +0200

    Add alpha_spaces validation rule

---
+++ src/validators.php
@@ -0,0 +1,7 @@
+<?php
+
+// As seen on http://blog.elenakolevska.com/laravel-alpha-validator-that-allows-spaces/
+Validator::extend('alpha_spaces', function($attribute, $value)
+{
+    return preg_match('/^[\pL\s]+$/u', $value);
+});
