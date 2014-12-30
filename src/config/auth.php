f073808108e93d87e26fa1e9bcca0ca7d35e5fdc
Ryan C. Durham <ryan@stagerightlabs.com>
2014-12-30 04:14:21 +0000

    Rename sentinel.php config file to auth.php

--- src/config/auth.php
+++ src/config/auth.php
@@ -66,17 +66,4 @@ return array(
 
     'allow_usernames' => true,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Layout
-    |--------------------------------------------------------------------------
-    |
-    | By default, the views provided by the package will extend their own
-    | default view (views/layouts/default.blade.php), even after they have been
-    | published.  This option allows you to extend a custom view instead.
-    |
-    */
-
-    'layout' => 'Sentinel::layouts.default',
-
 );
