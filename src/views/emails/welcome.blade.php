3b0216ee079e3cbca73ab2adbb45c3d3c80881e9
Ryan C. Durham <ryan@stagerightlabs.com>
2015-01-03 20:27:36 +0000

    Update controller actions in email templates

--- src/views/emails/welcome.blade.php
+++ src/views/emails/welcome.blade.php
@@ -7,8 +7,8 @@
 		<h2>Welcome</h2>
 
 		<p><b>Account:</b> {{{ $email }}}</p>
-		<p>To activate your account, <a href="{{ URL::action('Sentinel\UserController@activate', array('id' => $userId, 'code' => urlencode($activationCode))) }}">click here.</a></p>
-		<p>Or point your browser to this address: <br /> {{ URL::action('Sentinel\UserController@activate', array('id' => $userId, 'code' => urlencode($activationCode))) }} </p>
+		<p>To activate your account, <a href="{{ URL::action('Sentinel\RegistrationController@activate', array('id' => $userId, 'code' => urlencode($activationCode))) }}">click here.</a></p>
+		<p>Or point your browser to this address: <br /> {{ URL::action('Sentinel\RegistrationController@activate', array('id' => $userId, 'code' => urlencode($activationCode))) }} </p>
 		<p>Thank you, <br />
 			~The Admin Team</p>
 	</body>
