2911613d47fc7fc6bddb3ff7b6086e484136224e
Ryan C. Durham <ryan@stagerightlabs.com>
2015-01-04 20:54:31 +0000

    Add routes for modifying group memberships

--- src/routes.php
+++ src/routes.php
@@ -19,18 +19,21 @@ Route::post('register', ['as' => 'sentinel.register.user', 'uses' => 'Sentinel\R
 Route::get('users/activate/{id}/{code}', ['as' => 'sentinel.activate', 'uses' => 'Sentinel\RegistrationController@activate']);
 Route::get('reactivate', ['as' => 'sentinel.reactivate.form', 'uses' => 'Sentinel\RegistrationController@resendActivationForm']);
 Route::post('reactivate', ['as' => 'sentinel.resend.activation', 'uses' => 'Sentinel\RegistrationController@resendActivation']);
-
-// Profile
 Route::get('forgot', ['as' => 'sentinel.forgot.form', 'uses' => 'Sentinel\RegistrationController@forgotPasswordForm']);
 Route::post('forgot', ['as' => 'sentinel.reset.request', 'uses' => 'Sentinel\RegistrationController@sendResetPasswordEmail']);
 Route::get('reset/{id}/{code}', ['as' => 'sentinel.reset.form', 'uses' => 'Sentinel\RegistrationController@passwordResetForm']);
 Route::post('reset/{id}/{code}', ['as' => 'sentinel.reset.password', 'uses' => 'Sentinel\RegistrationController@resetPassword']);
 
+// Profile
+Route::post('users/{id}/password', ['as' => 'sentinel.change.password', 'uses' => 'Sentinel\UserController@changePassword']);
+
 // Users
 Route::get('users', ['as' => 'sentinel.users.index', 'uses' => 'Sentinel\UserController@index']);
-Route::post('users/add', ['as' => 'sentinel.users.add', 'uses' => 'Sentinel\UserController@add']);
+Route::get('users/create', ['as' => 'sentinel.users.create', 'uses' => 'Sentinel\UserController@create']);
+Route::post('users', ['as' => 'sentinel.users.store', 'uses' => 'Sentinel\UserController@store']);
 Route::get('users/{id}', ['as' => 'sentinel.users.show', 'uses' => 'Sentinel\UserController@show']);
 Route::get('users/{id}/edit', ['as' => 'sentinel.users.edit', 'uses' => 'Sentinel\UserController@edit']);
+Route::post('users/{id}/memberships', ['as' => 'sentinel.users.memberships', 'uses' => 'Sentinel\UserController@updateGroupMemberships']);
 Route::put('users/{id}', ['as' => 'sentinel.users.update', 'uses' => 'Sentinel\UserController@update']);
 Route::delete('users/{id}', ['as' => 'sentinel.users.destroy', 'uses' => 'Sentinel\UserController@destroy']);
 Route::get('users/{id}/suspend', ['as' => 'sentinel.users.suspend', 'uses' => 'Sentinel\UserController@suspend']);
@@ -38,7 +41,7 @@ Route::get('users/{id}/unsuspend', ['as' => 'sentinel.users.unsuspend', 'uses' =
 Route::get('users/{id}/ban', ['as' => 'sentinel.users.ban', 'uses' => 'Sentinel\UserController@ban']);
 Route::get('users/{id}/unban', ['as' => 'sentinel.users.unban', 'uses' => 'Sentinel\UserController@unban']);
 
-// Group Routes
+// Groups
 Route::get('groups', ['as' => 'sentinel.groups.index', 'uses' => 'Sentinel\GroupController@index']);
 Route::get('groups/create', ['as' => 'sentinel.groups.create', 'uses' => 'Sentinel\GroupController@create']);
 Route::post('groups', ['as' => 'sentinel.groups.store', 'uses' => 'Sentinel\GroupController@store']);
