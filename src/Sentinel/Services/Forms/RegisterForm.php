4ef57eb2719c36f7dcb60bbbfae141a8e0e781e7
Ryan C. Durham <ryan@stagerightlabs.com>
2015-01-12 03:34:29 +0000

    Conditionally add validation rules for usernames

--- src/Sentinel/Services/Forms/RegisterForm.php
+++ src/Sentinel/Services/Forms/RegisterForm.php
@@ -1,8 +1,16 @@
 <?php namespace Sentinel\Services\Forms;
 
+use Illuminate\Config\Repository;
+use Illuminate\Validation\Factory;
 use Sentinel\Services\Validation\FormValidator;
 
 class RegisterForm extends FormValidator {
+
+    public function __construct(Factory $validator, Repository $config)
+    {
+        $this->validator = $validator;
+        $this->config = $config;
+    }
 
     protected $rules = [
         'email' => 'required|min:4|max:254|email',
@@ -12,4 +20,18 @@ class RegisterForm extends FormValidator {
     ];
 
     protected $messages = [];
+
+    /**
+     * Get RegisterFrorm Validation Rules
+     * @return mixed
+     */
+    public function getRules()
+    {
+        // If Usernames are enabled, add username validation rules
+        if ($this->config->get('Sentinel::auth.allow_usernames')) {
+            $this->rules['username'] = 'required|unique:users,username';
+        }
+
+        return $this->rules;
+    }
 }
--- src/Sentinel/Services/Forms/UserCreateForm.php
+++ src/Sentinel/Services/Forms/UserCreateForm.php
@@ -1,15 +1,37 @@
 <?php namespace Sentinel\Services\Forms;
 
+use Illuminate\Config\Repository;
+use Illuminate\Validation\Factory;
 use Sentinel\Services\Validation\FormValidator;
 
 class UserCreateForm extends FormValidator {
+
+    public function __construct(Factory $validator, Repository $config)
+    {
+        $this->validator = $validator;
+        $this->config = $config;
+    }
 
     protected $rules = [
         'email' => 'required|min:4|max:254|email',
         'password' => 'required|min:6|confirmed',
         'password_confirmation' => 'required',
-        'username' => 'unique:users,username'
     ];
 
     protected $messages = [];
+
+
+    /**
+     * Get UserCreateForm Validation Rules
+     * @return mixed
+     */
+    public function getRules()
+    {
+        // If Usernames are enabled, add username validation rules
+        if ($this->config->get('Sentinel::auth.allow_usernames')) {
+            $this->rules['username'] = 'required|unique:users,username';
+        }
+
+        return $this->rules;
+    }
 }
