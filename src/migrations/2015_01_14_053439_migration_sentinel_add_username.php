de7ac2df34199f8a67d5151e70e8fbeedae103b4
Ryan C. Durham <ryan@stagerightlabs.com>
2015-01-14 06:39:46 +0000

    Move username to separate migration file

--- src/migrations/2015_01_14_053439_migration_sentinel_add_username.php
+++ src/migrations/2015_01_14_053439_migration_sentinel_add_username.php
@@ -5,24 +5,32 @@ use Illuminate\Database\Migrations\Migration;
 
 class MigrationSentinelAddUsername extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		//
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Add the username to the users table
+        Schema::table('users', function($table)
+        {
+            $table->string('username')->nullable()->unique();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		//
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Remove username from the users table
+        Schema::table('users', function($table)
+        {
+            $table->dropColumn('username');
+        });
+    }
 
 }
