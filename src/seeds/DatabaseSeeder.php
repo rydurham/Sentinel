d524f199d957610e9a2170d79abd21f2162a0373
Ryan C. Durham <ryan@stagerightlabs.com>
2015-01-31 05:55:11 +0000

    Add Seeder reference to seeder classes

--- src/seeds/DatabaseSeeder.php
+++ src/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
+
 class SentinelDatabaseSeeder extends Seeder {
 
 	/**
--- src/seeds/SentryUserGroupSeeder.php
+++ src/seeds/SentryUserGroupSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Database\Seeder;
+
 class SentryUserGroupSeeder extends Seeder {
 
 	/**
