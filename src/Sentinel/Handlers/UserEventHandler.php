4956a34c25f37221ec10f646e1d162de40e37492
Ryan C. Durham <ryan@stagerightlabs.com>
2015-02-01 07:16:09 +0000

    Consolidate user event listeners

--- src/Sentinel/Handlers/UserEventHandler.php
+++ src/Sentinel/Handlers/UserEventHandler.php
@@ -1,12 +1,31 @@
 <?php namespace Sentinel\Handlers;
 
 use Illuminate\Session\Store;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Contracts\Config\Repository;
 
-class UserEventHandler {
+class UserEventHandler
+{
 
-    public function __construct(Store $session)
+    public function __construct(Store $session, Repository $config)
     {
         $this->session = $session;
+        $this->config = $config;
+    }
+
+    /**
+     * Register the listeners for the subscriber.
+     *
+     * @param  \Illuminate\Events\Dispatcher $events
+     * @return array
+     */
+    public function subscribe($events)
+    {
+        $events->listen('sentinel.user.login', 'Sentinel\Handlers\UserEventHandler@onUserLogin', 10);
+        $events->listen('sentinel.user.logout', 'Sentinel\Handlers\UserEventHandler@onUserLogout', 10);
+        $events->listen('sentinel.user.registered', 'Sentinel\Handlers\UserMailer@welcome', 10);
+        $events->listen('sentinel.user.resend', 'Sentinel\Handlers\UserMailer@welcome', 10);
+        $events->listen('sentinel.user.reset', 'Sentinel\Handlers\UserMailer@passwordReset', 10);
     }
 
     /**
@@ -26,16 +45,66 @@ class UserEventHandler {
         $this->session->flush();
     }
 
+    /**
+     * Send a welcome email to a new user.
+     *
+     * @param $user
+     * @param $activated
+     *
+     * @return bool
+     * @internal param string $email
+     * @internal param int $userId
+     * @internal param string $activationCode
+     */
+    public function welcome($user, $activated)
+    {
+        $subject = $this->config('Sentinel::email.welcome');
+        $view = 'Sentinel::emails.welcome';
+        $data['hash'] = $user->hash;
+        $data['code'] = $user->getActivationCode();
+        $data['email'] = $user->email;
+
+        if (! $activated)
+        {
+            $this->sendTo( $user->email, $subject, $view, $data );
+        }
+
+    }
 
     /**
-     * Register the listeners for the subscriber.
+     * Email Password Reset info to a user.
      *
-     * @param  \Illuminate\Events\Dispatcher  $events
-     * @return array
+     * @param $user
+     * @param $code
+     *
+     * @internal param string $email
+     * @internal param int $userId
+     * @internal param string $resetCode
      */
-    public function subscribe($events)
+    public function passwordReset($user, $code)
     {
-        $events->listen('sentinel.user.login', 'Sentinel\Handlers\UserEventHandler@onUserLogin', 10);
-        $events->listen('sentinel.user.logout', 'Sentinel\Handlers\UserEventHandler@onUserLogout', 10);
+        $subject = $this->config('Sentinel::email.reset_password');
+        $view = 'Sentinel::emails.reset';
+        $data['hash'] = $user->hash;
+        $data['code'] = $code;
+        $data['email'] = $user->email;
+
+        $this->sendTo($user->email, $subject, $view, $data );
+    }
+
+    /**
+     * Convenience function for sending mail
+     *
+     * @param $email
+     * @param $subject
+     * @param $view
+     * @param array $data
+     */
+    private function sendTo($email, $subject, $view, $data = array())
+    {
+        Mail::queue($view, $data, function ($message) use ($email, $subject) {
+            $message->to($email)
+                ->subject($subject);
+        });
     }
 }
