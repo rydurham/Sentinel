c6720577ae90cced4946d04512cd3b879256beb5
Ryan C. Durham <ryan@stagerightlabs.com>
2015-02-07 22:56:55 +0000

    Switch filters to Middleware

---
+++ src/Sentinel/Middleware/SentryAdminAccess.php
@@ -0,0 +1,48 @@
+<?php namespace Sentinel\Middleware;
+
+use Closure, Session, Sentry;
+use Illuminate\Contracts\Routing\Middleware;
+
+class SentryAdminAccess implements Middleware {
+
+    /**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+        // First make sure there is an active session
+        if ( ! Sentry::check())
+        {
+            if ($request->ajax())
+            {
+                return response('Unauthorized.', 401);
+            }
+            else
+            {
+                return redirect()->guest(route('sentinel.login'));
+            }
+        }
+
+        // Now check to see if the current user has the 'admin' permission
+        if ( ! Sentry::getUser()->hasAccess('admin'))
+        {
+            if ($request->ajax())
+            {
+                return response('Unauthorized.', 401);
+            }
+            else
+            {
+                Session::flash('error', trans('Sentinel::users.noaccess'));
+                return redirect()->route('sentinel.login');
+            }
+        }
+
+        // All clear - we are good to move forward
+        return $next($request);
+	}
+
+}
---
+++ src/Sentinel/Middleware/SentryAuth.php
@@ -0,0 +1,32 @@
+<?php namespace Sentinel\Middleware;
+
+use Closure, Sentry;
+use Illuminate\Contracts\Routing\Middleware;
+
+class SentryAuth implements Middleware {
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+        if ( ! Sentry::check())
+        {
+            if ($request->ajax())
+            {
+                return response('Unauthorized.', 401);
+            }
+            else
+            {
+                return redirect()->guest(route('sentinel.login'));
+            }
+        }
+
+        return $next($request);
+	}
+
+}
