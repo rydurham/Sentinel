eb2df7f33ffb2fce7ce243a601f0996818d05215
Ryan C. Durham <ryan@stagerightlabs.com>
2015-02-07 23:11:38 +0000

    Update Profile Controller methods for L5

--- src/controllers/ProfileController.php
+++ src/controllers/ProfileController.php
@@ -2,13 +2,16 @@
 
 use Hashids\Hashids;
 use Illuminate\Routing\Controller as BaseController;
+use Sentinel\FormRequests\ChangePasswordRequest;
+use Sentinel\FormRequests\UserUpdateRequest;
 use Session, Input, Response, Redirect;
 use Sentinel\Repositories\Group\SentinelGroupRepositoryInterface;
 use Sentinel\Repositories\User\SentinelUserRepositoryInterface;
 use Sentinel\Traits\SentinelRedirectionTrait;
 use Sentinel\Traits\SentinelViewfinderTrait;
 
-class ProfileController extends BaseController {
+class ProfileController extends BaseController
+{
 
     /**
      * Traits
@@ -20,40 +23,38 @@ class ProfileController extends BaseController {
         SentinelUserRepositoryInterface $userRepository,
         SentinelGroupRepositoryInterface $groupRepository,
         Hashids $hashids
-    )
-    {
+    ) {
         // DI Member assignment
-        $this->userRepository = $userRepository;
+        $this->userRepository  = $userRepository;
         $this->groupRepository = $groupRepository;
-        $this->hashids = $hashids;
+        $this->hashids         = $hashids;
 
         // You must have an active session to proceed
-        $this->middleware('sentry.admin');
-
+        $this->middleware('sentry.auth');
     }
 
     /**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show()
-	{
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show()
+    {
         // Get the user
         $user = $this->userRepository->retrieveById(Session::get('userId'));
 
         return $this->viewFinder('Sentinel::users.show', ['user' => $user]);
-	}
+    }
 
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @return Response
-	 */
-	public function edit()
-	{
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @return Response
+     */
+    public function edit()
+    {
         // Get the user
         $user = $this->userRepository->retrieveById(Session::get('userId'));
 
@@ -64,43 +65,37 @@ class ProfileController extends BaseController {
             'user' => $user,
             'groups' => $groups
         ]);
-	}
+    }
 
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function update()
-	{
+    /**
+     * Update the specified resource in storage.
+     *
+     * @return Response
+     */
+    public function update(UserUpdateRequest $request)
+    {
         // Gather Input
-        $data = Input::all();
+        $data       = Input::all();
         $data['id'] = Session::get('userId');
-
-        // Validate form data
-        $this->userUpdateForm->validate($data);
 
         // Attempt to update the user
         $result = $this->userRepository->update($data);
 
         // Done!
-        return $this->redirectViaResponse('profile.update', $result);
-	}
+        return $this->redirectViaResponse('profile_update', $result);
+    }
 
     /**
      * Process a password change request
      *
      * @return Redirect
      */
-    public function changePassword()
+    public function changePassword(ChangePasswordRequest $request)
     {
         // Gather input
         $data       = Input::all();
         $data['id'] = Session::get('userId');
-
-        // Validate form Data
-        $this->changePasswordForm->validate($data);
 
         // Grab the current user
         $user = $this->userRepository->getUser();
@@ -109,12 +104,13 @@ class ProfileController extends BaseController {
         $result = ($user->hasAccess('admin') ? $this->userRepository->changePasswordWithoutCheck($data) : $this->userRepository->changePassword($data));
 
         // Was the change successful?
-        if (! $result->isSuccessful())
-        {
+        if (!$result->isSuccessful()) {
             Session::flash('error', $result->getMessage());
+
             return Redirect::back();
         }
-        return $this->redirectViaResponse('profile.change.password', $result);
+
+        return $this->redirectViaResponse('profile_change_password', $result);
     }
 
 }
