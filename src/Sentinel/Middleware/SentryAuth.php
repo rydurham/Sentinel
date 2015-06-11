8cc8945d2c0218c0be16b617bb41553e29899e84
Ryan C. Durham <ryan@stagerightlabs.com>
2015-06-11 21:05:42 +0000

    Add Sentry Group Membership middleware

--- src/Sentinel/Middleware/SentryAuth.php
+++ src/Sentinel/Middleware/SentryAuth.php
@@ -3,30 +3,27 @@
 use Closure, Sentry;
 use Illuminate\Contracts\Routing\Middleware;
 
-class SentryAuth implements Middleware {
+class SentryAuth implements Middleware
+{
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-        if ( ! Sentry::check())
-        {
-            if ($request->ajax())
-            {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!Sentry::check()) {
+            if ($request->ajax()) {
                 return response('Unauthorized.', 401);
-            }
-            else
-            {
+            } else {
                 return redirect()->guest(route('sentinel.login'));
             }
         }
 
         return $next($request);
-	}
+    }
 
 }
---
+++ src/Sentinel/Middleware/SentryMember.php
@@ -0,0 +1,44 @@
+<?php namespace Sentinel\Middleware;
+
+use Closure, Sentry;
+use Illuminate\Contracts\Routing\Middleware;
+
+class SentryMember implements Middleware
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param string $group
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $group)
+    {
+        if (!Sentry::check()) {
+            if ($request->ajax()) {
+                return response('Unauthorized.', 401);
+            } else {
+                return redirect()->guest(route('sentinel.login'));
+            }
+        }
+
+        // Find the specified group
+        $group = Sentry::findGroupByName($group);
+
+        // Now check to see if the current user is a member of the specified group
+        if (!Sentry::getUser()->inGroup($group)) {
+            if ($request->ajax()) {
+                return response('Unauthorized.', 401);
+            } else {
+                Session::flash('error', trans('Sentinel::users.noaccess'));
+
+                return redirect()->route('sentinel.login');
+            }
+        }
+
+        return $next($request);
+    }
+
+}
