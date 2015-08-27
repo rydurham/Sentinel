35673360a036f18c9520d965c4d43ef02c18afe0
Ryan Durham <ryan@stagerightlabs.com>
2015-08-26 20:58:54 -0400

    Applied fixes from StyleCI

--- src/lang/fr/groups.php
+++ src/lang/fr/groups.php
@@ -2,26 +2,26 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Group Repositiory Messages
-	|--------------------------------------------------------------------------
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Group Repositiory Messages
+    |--------------------------------------------------------------------------
+    */
 
-	'created' 		=> "Groupe créé avec succès.",	
+    'created'        => "Groupe créé avec succès.",
 
-	'loginreq' 		=> "Champ de connexion requis.",
+    'loginreq'        => "Champ de connexion requis.",
 
-	'userexists' 	=> "L'utilisateur existe déjà.",
+    'userexists'    => "L'utilisateur existe déjà.",
 
-	'updated' 		=> "Groupe a été mis à jour.",
+    'updated'        => "Groupe a été mis à jour.",
 
-	'updateproblem' => "Il y a eu un problème de mise à jour du groupe.",
+    'updateproblem' => "Il y a eu un problème de mise à jour du groupe.",
 
-	'namereq' 		=> "Vous devez fournir un nom de groupe.",
+    'namereq'        => "Vous devez fournir un nom de groupe.",
 
-	'groupexists' 	=> "Ce groupe existe déjà.",
+    'groupexists'    => "Ce groupe existe déjà.",
 
-	'notfound' 		=> "Groupe non trouvé.",
+    'notfound'        => "Groupe non trouvé.",
 
 );
